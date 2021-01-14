c369a6dc63feb741be4d7c8268da7d85c9ea0449
João Cavacos <cavacosdoesgaming@gmail.com>
2021-01-14 04:30:15 +0000

    Platform timer disappear script
    Finished the script for the first level - memory loss. Where you have to jump on platform until you reach the end, but you will have a timer of 30 seconds that will make the tiles disappear if it reaches zero. Activates through a pressure plate

---
+++ LostFound/Assets/Scripts/DisablePlatform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisablePlatform : MonoBehaviour
+{
+
+	public GameObject levels;
+
+	private bool timerStart = false;
+
+	private float timer = 30f;
+
+	public Text timerText;
+
+
+	void Update()
+	{
+		timerText.text = (Mathf.RoundToInt(timer)).ToString();
+		
+		if(timerStart == true)
+		{
+			timer -= Time.deltaTime;
+		}
+		else if(timerStart == false)
+		{
+			timer = 30f;
+		}
+
+		if(timer <= 0f)
+		{
+			levels.SetActive(false);
+			timer = 0f;
+		}
+		else
+		{
+			levels.SetActive(true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("StartTimer"))
+		{
+			timerStart = true;
+		}
+
+		if (other.CompareTag("ResetTimer"))
+		{
+			timerStart = false;
+		}
+	}
+
+
+}
